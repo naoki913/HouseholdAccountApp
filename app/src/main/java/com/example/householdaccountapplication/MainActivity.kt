cad498e7790a8dcf6aae0c5e9c7f075b22e2050f
naoki913 <mr.kanoi3190@gmail.com>
2019-06-23 14:32:35 +0900

    【Add】スクロール機能を追加

--- app/src/main/java/com/example/householdaccountapplication/MainActivity.kt
+++ app/src/main/java/com/example/householdaccountapplication/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val spinnerItems=arrayOf("食事","娯楽")
+    private var spinnerItems=arrayOf("食事","娯楽")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,46 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, spinnerItems)
 
-        //var temp1:Int=10;
-        //var temp2:Int
-        //var minus:ArrayList<Int> =arrayListOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-
         val vg = findViewById<View>(R.id.tableLayout) as ViewGroup
-        /*
-        for (i in 0 until 5 ){
-            getLayoutInflater().inflate(R.layout.table, vg)
-            val tr = vg.getChildAt(i) as TableRow;
 
-            //spinnerの処理
-            ((tr.getChildAt(0))as Spinner).adapter = adapter
-            //textviewに文字を格納
-            ((tr.getChildAt(1)) as EditText).setText(i.toString())
-            ((tr.getChildAt(2)) as EditText).setText(i.toString())
-            ((tr.getChildAt(3)) as Button)?.setText("削除")
-            //buttonの動的追加と押されたときの処理の記載
-            ((tr.getChildAt(3)) as Button)?.setOnClickListener {
-                //この中に処理を書きます
-                //println(((tr.getChildAt(1)) as Button).id)
-                temp2=i
-
-                for(j in 0 until 5){
-                    if(temp1<j){
-                        minus[j]++
-
-                    }
-                }
-                    temp2 -= minus[i]
-                vg.removeViewAt(temp2)
-                temp1=i
-            }
-
-        }
-        */
         var k:Int=0
 
-        button_aaa.setOnClickListener{
+        button_add.setOnClickListener{
 
-            println("aaa")
             getLayoutInflater().inflate(R.layout.table, vg)
             val tr = vg.getChildAt(k) as TableRow;
 
@@ -68,24 +34,7 @@ class MainActivity : AppCompatActivity() {
             ((tr.getChildAt(3)) as Button)?.setText("削除")
             //buttonの動的追加と押されたときの処理の記載
             ((tr.getChildAt(3)) as Button)?.setOnClickListener {
-                //この中に処理を書きます
-                //println(((tr.getChildAt(1)) as Button).id)
-                /*
 
-                temp2=k
-
-                for(j in 0 until 6){
-                    if(temp1<j){
-                        minus[j]++
-
-                    }
-                }
-                temp2 -= minus[k]
-                vg.removeViewAt(temp2)
-                minus.removeAt(temp2)
-                k--
-                temp1=k
-            }*/
                 vg.removeView(tr)
                 k--
             }
