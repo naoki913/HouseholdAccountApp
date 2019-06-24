cefede2ee657e6dd78aaef5d834b42cd03030890
NAOKIY <mr.kanoi3190@gmail.com>
2019-06-24 21:58:38 +0900

    【Add】ジャンルごとに合計金額を表示する機能を追加

--- app/src/main/java/com/example/householdaccountapplication/TotalActivity.kt
+++ app/src/main/java/com/example/householdaccountapplication/TotalActivity.kt
@@ -1,12 +1,12 @@
 package com.example.householdaccountapplication
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 
 class TotalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +19,16 @@ class TotalActivity : AppCompatActivity() {
         var spinnerItems=intent.getStringArrayListExtra("SPINNER_ITEMS_KEY")
         var sum=intent.getIntegerArrayListExtra("SUM_KEY")
         println("sum="+sum)
+        var texts: Array<TextView?> = arrayOfNulls(spinnerItems.size)
+        val layout: FrameLayout = findViewById(R.id.totalTableLayout)
 
-
-
-
-
-
-
-
-
+        for (i in 0..spinnerItems.size-1){
+            texts[i] = TextView(this)
+            texts[i]?.text = spinnerItems[i]+"　：　"+sum[i]+"円"
+            texts[i]?.setTextSize(30f)
+            texts[i]?.y =50.0f + 150.0f*i
+            layout.addView( texts[i], LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 150*spinnerItems.size))
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
